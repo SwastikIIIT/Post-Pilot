2af1813a863cfc1b84b0171f0687b0d22c663148
Swastik Sharma <153355864+SwastikIIIT@users.noreply.github.com>
2025-03-04 22:44:17 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const UserSchema=new Schema(
     {
           email:{
               type:String,
-              required:[true,"Email is required"],
+              required:[true,"Email is "],
               unique:[true,"Email already exits"]
           },
           username:{
